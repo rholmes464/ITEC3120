39e9e5197fb9b783f3a1287f982f3a3c46c4904a
richardholmes <richardholmes@10.0.1.10>
2014-07-12 19:30:08 -0600

    updated from the laptop

--- src/itec3120/mainAbstract.java
+++ src/itec3120/mainAbstract.java
@@ -39,22 +39,27 @@ public abstract class mainAbstract {
     
     public void SetLN(String Last)
     {
+        //this sets the last name - so that sub-classes can call it
         LastName = Last;
     }
     public void SetFN(String First) 
     {
+        //this sets the first name
         FirstName = First;
     }
     public void SetAdd (String Add)
     {
+        //this sets the address
         Address = Add;
     }
     public void SetPN(String PN)
     {
+        // this sets the phone number
         PhoneNumber = PN;
     }
     public void SetEM (String EM)
     {
+        //this sets the Email
         Email = EM;
     }
     public void SetCount(int arCount)
@@ -63,22 +68,27 @@ public abstract class mainAbstract {
     }
     public String getLastName()
     {
+        //gets the lastname
         return LastName;
     }
     public String getFirstName()
     {
+        //gets the first name
         return FirstName;
     }
     public String getAddress()
     {
+        //gets the address
         return Address;
     }
     public String getPhoneNumber()
     {
+        //gets the phone number
         return PhoneNumber;
     }
     public String getEmail() 
     {
+        //gets the email
         return Email;
     }
   /*  public int getCount()
@@ -88,6 +98,7 @@ public abstract class mainAbstract {
     @Override
     public String toString()
     {
+        // this is called by the subclasses so that they can print out the fields
         return String.format("%s %s %s", getLastName(),getFirstName(),printArray(bill));
     }
     public void printContacts(ArrayList arl)
